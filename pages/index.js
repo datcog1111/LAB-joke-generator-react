3b839e0b501930efa93f454a7ac478d5e55c972e
datcog1111 <h9tuck@gmail.com>
2023-07-11 20:04:01 -0500

    finished successfully

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,43 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import getJoke from '../api/jokeData';
+import Jokes from '../components/Joke';
+
 function Home() {
+  // allowing button to change text with useState, the initial set state is "get a joke"
+  const [btnText, setBtnText] = useState('Get A Joke');
+  // setting an empty use state for the joke API call
+  const [joke, setJoke] = useState({});
+
+  const buttonText = (text) => {
+    setBtnText(text);
+  };
+
+  const jokies = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        punchline: obj.delivery,
+      });
+
+      buttonText('Get Punchline');
+    });
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
-    </div>
+    <>
+      <Jokes joke={joke} btnText={btnText} />
+      {btnText === 'Get A Joke' || btnText === 'Get Another Joke' ? (
+        <Button type="button" onClick={jokies}>
+          {btnText}
+        </Button>
+      ) : (
+        <Button type="button" onClick={() => buttonText('Get Another Joke')}>
+          {btnText}
+        </Button>
+      )}
+
+    </>
   );
 }
 
